c544466c51d1d7052237097fd4ec8722ff5118df
Shaw <shawmakesmagic@gmail.com>
2026-02-11 15:56:58 -0800

    app/electron: localize permissions types to fix TS6059

--- apps/app/electron/src/native/permissions-darwin.ts
+++ apps/app/electron/src/native/permissions-darwin.ts
@@ -16,7 +16,7 @@
 import { exec } from "child_process";
 import { promisify } from "util";
 import { systemPreferences, desktopCapturer, shell } from "electron";
-import type { PermissionCheckResult, SystemPermissionId } from "../../../../../src/permissions/types.js";
+import type { PermissionCheckResult, SystemPermissionId } from "./permissions-shared.js";
 
 const execAsync = promisify(exec);
 
